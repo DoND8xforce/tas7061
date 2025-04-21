2ae1b61d9ea63940998f4e6386f1d46f81888bab
DoND8xforce <dond8@xforcecorp.com>
2025-04-21 23:07:02 +0700

    Redesign layout tas7061if and tas7061bb

--- tas7061if/widgets/wgContentSmb100B/wgContentSmb100B.cpp
+++ tas7061if/widgets/wgContentSmb100B/wgContentSmb100B.cpp
@@ -4,6 +4,12 @@
 wgContentSmb100B::wgContentSmb100B(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::wgContentSmb100B)
+    , mWgRfFreqShow(true)
+    , mWgPulseModShow(true)
+    , mWgRfLevelShow(true)
+    , mWgAdvPulseModShow(true)
+    , mWgAdvRfShow(true)
+    , mWgAdvRfLevelShow(true)
 {
     ui->setupUi(this);
 }
@@ -12,3 +18,45 @@ wgContentSmb100B::~wgContentSmb100B()
 {
     delete ui;
 }
+
+void wgContentSmb100B::on_btnRfFreq_clicked()
+{
+    mWgRfFreqShow ^= true;
+    ui->wgRfFreq->setVisible(mWgRfFreqShow);
+}
+
+
+void wgContentSmb100B::on_btnAdvRfFreq_clicked()
+{
+    mWgAdvRfShow ^= true;
+    ui->wgAdvRfFreq->setVisible(mWgAdvRfShow);
+}
+
+
+void wgContentSmb100B::on_btnPulseMod_clicked()
+{
+    mWgPulseModShow ^= true;
+    ui->wgPulseMod->setVisible(mWgPulseModShow);
+}
+
+
+void wgContentSmb100B::on_btnAdvPulse_clicked()
+{
+    mWgAdvPulseModShow ^= true;
+    ui->wgAdvPulseMod->setVisible(mWgAdvPulseModShow);
+}
+
+
+void wgContentSmb100B::on_btnRfLevel_clicked()
+{
+    mWgRfLevelShow ^= true;
+    ui->wgRfLevel->setVisible(mWgRfLevelShow);
+}
+
+
+void wgContentSmb100B::on_btnAdvRfLevel_clicked()
+{
+    mWgAdvRfLevelShow ^= true;
+    ui->wgAdvRfLevel->setVisible(mWgAdvRfLevelShow);
+}
+
--- tas7061if/widgets/wgContentSmb100B/wgContentSmb100B.h
+++ tas7061if/widgets/wgContentSmb100B/wgContentSmb100B.h
@@ -15,8 +15,27 @@ public:
     explicit wgContentSmb100B(QWidget *parent = nullptr);
     ~wgContentSmb100B();
 
+private slots:
+    void on_btnRfFreq_clicked();
+
+    void on_btnAdvRfFreq_clicked();
+
+    void on_btnPulseMod_clicked();
+
+    void on_btnAdvPulse_clicked();
+
+    void on_btnRfLevel_clicked();
+
+    void on_btnAdvRfLevel_clicked();
+
 private:
     Ui::wgContentSmb100B *ui;
+    bool mWgRfFreqShow;
+    bool mWgPulseModShow;
+    bool mWgRfLevelShow;
+    bool mWgAdvPulseModShow;
+    bool mWgAdvRfShow;
+    bool mWgAdvRfLevelShow;
 };
 
 #endif // WGCONTENTSMB100B_H
